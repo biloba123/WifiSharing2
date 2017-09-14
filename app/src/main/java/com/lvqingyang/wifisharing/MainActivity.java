a33ad4a904d6f689e7c63067e5dbd55acfc0eb96
biloba <biloba12345@gmail.com>
2017-09-14 16:54:32 +0800

    kk

--- app/src/main/java/com/lvqingyang/wifisharing/MainActivity.java
+++ app/src/main/java/com/lvqingyang/wifisharing/MainActivity.java
@@ -171,7 +171,7 @@ public class MainActivity extends BaseActivity {
     private void showRequestPermissionDialog(){
         View view=getLayoutInflater().inflate(R.layout.dialog_request_permission,null);
         MyDialog dialog=new MyDialog(this)
-                .setTitle(R.string.title_map)
+                .setTitle(R.string.feedback)
                 .setView(view)
                 .setPosBtn(android.R.string.ok, new View.OnClickListener() {
                     @Override
