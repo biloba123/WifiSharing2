0316b00c001a4e4474c5255dc1b8739775b4a1ce
biloba <biloba12345@gmail.com>
2017-09-25 19:42:59 +0800

    route complete

--- app/src/main/java/com/lvqingyang/wifisharing/Map/MapFragment.java
+++ app/src/main/java/com/lvqingyang/wifisharing/Map/MapFragment.java
@@ -291,7 +291,7 @@ public class MapFragment extends BaseFragment implements LocationSource, AMap.On
             @Override
             public void run() {
                 try {
-                    Thread.sleep(1500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
